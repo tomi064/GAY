93318b18932c9964c224bc6501f96aeca8a4810d
Bomas Beister <tffmeister@gmail.com>
2023-03-02 21:38:08 +0100

    init

---
+++ fetch_streamlink.ps1
@@ -0,0 +1,7 @@
+####
+#Gewünschten Film eingeben (Underscore als Trennzeichen)
+#fetcht Streamlink von kinox, womöglich muss man mehrere Anbieter abgrasen, diese haben IDs
+#durchgehen der Mirrors, bis einer funktioniert
+#myjdownloader integration, um nur die audio zu laden
+###
+
